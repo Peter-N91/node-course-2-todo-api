fa3173da4278abba360ae2e0ddc5b8ce5e5c8e95
Peter Nehme <peternehme91@hotmail.com>
2018-08-23 23:51:21 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -29,6 +30,27 @@ app.get('/todos', (req, res) => {
     res.status(400).send(err);
   });
 });
+
+
+//Fetch individual
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  //challenge
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.status(200).send(todo); // or res.status(200).send({todo}) or res.status(200).send({todo: todo})
+  }, (e) => {
+    res.status(400).send();
+  })
+});
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');

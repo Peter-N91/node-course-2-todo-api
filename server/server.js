60fa29103572a07c7e7f517ac3d24574893bd4ef
Peter Nehme <peternehme91@hotmail.com>
2018-08-23 15:17:02 +0300

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -20,8 +20,11 @@ app.post('/todos', (req, res) => {
   }, (err) => {
     res.status(400).send(err);
   })
-})
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+
+module.exports = {app};

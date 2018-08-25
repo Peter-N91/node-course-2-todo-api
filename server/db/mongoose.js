20dfa9493112c0255580efbf6cbbf53c65e39b3d
Peter Nehme <peternehme91@hotmail.com>
2018-08-25 20:56:27 +0300

    Setup separate test and developmen envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI); // removed because added in config.js for dev and test
 
 module.exports = {mongoose};

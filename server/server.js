7f13eeba4729d0eedb701f38f9b4a850010e67bf
Peter Nehme <peternehme91@hotmail.com>
2018-08-25 18:00:48 +0300

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.get('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.status(200).send(todo); // or res.status(200).send({todo}) or res.status(200).send({todo: todo})
+    res.status(200).send({todo}); // or res.status(200).send(todo) or res.status(200).send({todo: todo})
   }, (e) => {
     res.status(400).send();
   })
